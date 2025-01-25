26344495d7f9993d145c6e3a992d15fd8f2540c1
Abhishek jana <abhishekjana@Abhisheks-MacBook-Pro.local>
2025-01-25 20:08:36 +0530

    chief engineer commit

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { Box, Grid } from "@mui/material";
+import ProductImage from "../components/productDetailComponent/ProductImage";
+import ProductDetails from "../components/productDetailComponent/ProductDetails";
 
-function ProductPage() {
+import { useDispatch } from "react-redux";
+import { findProductByProductId } from "../store/actions/productActions";
+import { data } from "react-router-dom";
+
+const ProductPage = () => {
+  const dispatch = useDispatch();
+  
+  
+  useEffect(() => {
+    const dumyData = {
+      
+  };
+      dispatch(findProductByProductId());
+      
+    }, []);
+
   return (
-    <div>
-      product page
-    </div>
+    <Box
+      sx={{
+        bgcolor: "background.default",
+        color: "text.primary",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        p: { xs: 2, sm: 4 },
+      }}
+    >
+      <Grid
+        container
+        spacing={4}
+        sx={{
+          maxWidth: "1200px",
+          width: "100%",
+        }}
+      >
+        {/* Image Section */}
+        <Grid item xs={12} md={6}>
+          <ProductImage />
+        </Grid>
+
+        {/* Product Details Section */}
+        <Grid item xs={12} md={6}>
+          <ProductDetails />
+          
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
